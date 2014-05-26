ada1ca70131643c30a3d7f7c11c245d2034c4e66
baldaz <a.g.baldan@gmail.com>
2014-05-26 21:27:58 +0200

    Ibu to resolve

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require "sinatra"
 require "sinatra/activerecord"
 require "sinatra/contrib"
 require "./environments"
+require_relative "lib/calculations/bitterness"
 
 class Hops < ActiveRecord::Base
 end
@@ -18,6 +19,10 @@ helpers do
     else
       "Brewby"
     end
+  end
+
+  def ibu_result
+    erb :"calculations/_ibu_result"
   end
 end
 
@@ -43,4 +48,23 @@ get "/calculations" do
   @styles = Styles.order("name ASC")
   @hops = Hops.order("name ASC")
   erb :"calculations/index"
+end
+
+get "/calculations/ibu" do
+  hop_grams = params[:hop_grams]
+  alpha_acid_percentage = params[:alpha_acid]
+  boil_time = params[:boil_time]
+  batch_liters = params[:batch_size]
+
+  rg = Rager.new
+  util_percentage = rg.util_percentage(boil_time.to_i)
+#  @output = rg.ibu(hop_grams.to_i, util_percentage, alpha_acid_percentage.to_i, batch_liters.to_i, 0)
+  ts = Tinseth.new
+  b_t_factor = ts.boil_time_factor(boil_time.to_i)
+  b_factor = ts.bigness_factor(1050)
+  aa_util = ts.alpha_acid_utilization(b_factor, b_t_factor)
+  mg_aa = ts.mg_alpha_acids(alpha_acid_percentage.to_i, hop_grams.to_i, batch_liters.to_i)
+#  @output = ts.ibu(aa_util, mg_aa)
+  @output = b_factor
+  erb :"calculations/_ibu_result", :layout => false
 end
--- lib/calculations/bitterness.rb
+++ lib/calculations/bitterness.rb
@@ -2,8 +2,51 @@
 
 class Rager
   
+  def initialize
+  end
+
   def ibu(hop_grams, util_percentage, alpha_acid_percentage, batch_liters, adjustment)
     ((hop_grams * util_percentage * alpha_acid_percentage * 1000) /
             (batch_liters * (1 + adjustment)))
   end
+
+  def util_percentage(minutes)
+    18.11 + 13.86 * Math.tanh((minutes - 31.32) / 18.27)
+  end
+  
+  def adjustment_factor(boil_gravity)
+    if boil_gravity > 1050
+      (boil_gravity - 1050) / 0.2
+    else
+      0
+    end
+  end
+  
 end
+
+class Tinseth
+
+  def initialize
+  end
+
+  def ibu(alpha_acid_utilization, mg_alpha_acids)
+    alpha_acid_utilization * mg_alpha_acids
+  end
+
+  def mg_alpha_acids(alpha_acid, hop_grams, batch_liters)
+    alpha_acid * hop_grams * 1000 / batch_liters
+  end
+
+  def alpha_acid_utilization(bigness_factor, boil_time_factor)
+    bigness_factor * boil_time_factor
+  end
+
+  def bigness_factor(wort_gravity)
+    1.65 * 0.000125 ** (wort_gravity.to_f - 1.0)
+  end
+
+  def boil_time_factor(time)
+    1 - Math.exp((-0.04 * time.to_f) / 4.15)
+  end
+end
+
